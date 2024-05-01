d925ffe11a7b3c612a75511b41439ff02cb99c88
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-05-01 21:04:24 +0300

    classes

---
+++ Classes/circle.js
@@ -0,0 +1,41 @@
+class Circle {
+    constructor(radius) {
+        this._radius = radius;
+    }
+
+    get radius() {
+        return this._radius;
+    }
+
+    set radius(newRadius) {
+        this._radius = newRadius;
+    }
+
+    get diameter() {
+        return this._radius * 2;
+    }
+
+    set diameter(newDiameter) {
+        this._radius = newDiameter / 2;
+    }
+
+    get area() {
+        return Math.PI * this._radius ** 2;
+    }
+}
+
+let c = new Circle(2);
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
+
+c.diameter = 1.6;
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
